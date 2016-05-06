925b3f5d09fba2570c1b7cd2b05669269675873c
Michael A. Piskuric <MPiskuric@int.rpmtec.com>
2016-05-06 10:46:07 -0400

    Missed a bunch of stuff last time

---
+++ MiskoFinanceWeb/Data/Viewed/VwDatabaseConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using MiskoPersist.Attributes;
+using MiskoPersist.Data;
+using MiskoPersist.Enums;
+
+namespace MiskoFinanceWeb.Data.Viewed
+{
+	public class VwDatabaseConnection : ViewedData
+	{
+		#region Viewed Properties
+		
+		[Viewed]
+		public String Name { get; set; }
+		[Viewed]
+		public DatabaseType DatabaseType { get; set; }
+		[Viewed]
+		public String Server { get; set; }
+		[Viewed]
+		public Int32? Port { get; set; }
+		[Viewed]
+		public String Datasource { get; set; }
+		[Viewed]
+		public String Username { get; set; }
+		[Viewed]
+		public String Password { get; set; }
+		[Viewed]
+		public String ConnectionString { get; set; }
+		[Viewed]
+		public ConnectionState State { get; set; }
+		[Viewed]
+		public ErrorMessages Errors { get; set; }
+		
+		#endregion
+	}
+}
---
+++ MiskoFinanceWeb/Data/Viewed/VwDatabaseConnections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MiskoPersist.Data;
+
+namespace MiskoFinanceWeb.Data.Viewed
+{
+	public class VwDatabaseConnections : ViewedDataList
+	{
+		public VwDatabaseConnections()
+			: base(typeof(VwDatabaseConnection))
+		{
+		}
+	}
+}
